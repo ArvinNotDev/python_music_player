607d02a9ee9076cabf35c84a3162fb69e597f2d6
ArvinNotDev <Arveenjafari19@gmail.com>
2025-05-27 18:50:29 +0330

    deleting unneccessary things from main.py

--- main.py
+++ main.py
@@ -1,9 +1,3 @@
-import sys
-import os
-
-# Add the parent directory (python_music_player) to sys.path
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-
 from ui.ui_manager import start_ui
 
 def main():
